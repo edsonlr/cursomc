cddc9a3e6866552eff36a7b62bdf3178d88042d7
edsonlr <elribeirom10@gmail.com>
2021-11-16 15:57:58 -0300

    Ajustes no endpoint/Categorias/{id}

--- src/main/java/com/edsonlr/cursomc/services/CategoriaService.java
+++ src/main/java/com/edsonlr/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.edsonlr.cursomc.domain.Categoria;
 import com.edsonlr.cursomc.repositories.CategoriaRepository;
+import com.edsonlr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,9 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
